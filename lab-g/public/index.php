a2c2edf8860385b4666ee4a4a36133cd2bcaad1f
pestosz <bz912yt@gmail.com>
2026-05-27 19:53:27 +0200

    lab-g progress

--- lab-g/public/index.php
+++ lab-g/public/index.php
@@ -38,6 +38,35 @@ switch ($action) {
         $controller = new \App\Controller\PostController();
         $view = $controller->deleteAction($_REQUEST['id'], $router);
         break;
+    case 'book-index':
+        $controller = new \App\Controller\BookController();
+        $view = $controller->indexAction($templating, $router);
+        break;
+    case 'book-create':
+        $controller = new \App\Controller\BookController();
+        $view = $controller->createAction($_REQUEST['book'] ?? null, $templating, $router);
+        break;
+    case 'book-edit':
+        if (! $_REQUEST['id']) {
+            break;
+        }
+        $controller = new \App\Controller\BookController();
+        $view = $controller->editAction($_REQUEST['id'], $_REQUEST['book'] ?? null, $templating, $router);
+        break;
+    case 'book-show':
+        if (! $_REQUEST['id']) {
+            break;
+        }
+        $controller = new \App\Controller\BookController();
+        $view = $controller->showAction($_REQUEST['id'], $templating, $router);
+        break;
+    case 'book-delete':
+        if (! $_REQUEST['id']) {
+            break;
+        }
+        $controller = new \App\Controller\BookController();
+        $view = $controller->deleteAction($_REQUEST['id'], $router);
+        break;
     case 'info':
         $controller = new \App\Controller\InfoController();
         $view = $controller->infoAction();
---
+++ lab-g/sql/02-book.sql
@@ -0,0 +1,6 @@
+CREATE TABLE book (
+                      id INTEGER PRIMARY KEY AUTOINCREMENT,
+                      title TEXT NOT NULL,
+                      author TEXT NOT NULL,
+                      description TEXT
+);
